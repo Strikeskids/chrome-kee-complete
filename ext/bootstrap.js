847eb66295cc515cfa5df752e9ed7787380eb4d7
Strikeskids <github@strikeskids.com>
2016-03-22 21:09:39 -0400

    Setup extension bootstrapping

---
+++ ext/bootstrap.js
@@ -0,0 +1,18 @@
+chrome.runtime.onInstalled.addListener(setup)
+
+function setup() {
+    const passwordFieldRule = {
+        conditions: [
+            new chrome.declarativeContent.PageStateMatcher({
+                css: ["input[type='password']"],
+            }),
+        ],
+        actions: [
+            new chrome.declarativeContent.ShowPageAction(),
+        ],
+    }
+
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([passwordFieldRule])
+    })
+}
