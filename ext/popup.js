989b5d745385c71df11661d0174a2ac4bff02585
Strikeskids <github@strikeskids.com>
2017-01-10 20:26:58 -0500

    Loading elm in extension

--- ext/popup.js
+++ ext/popup.js
@@ -1,1 +1,5 @@
-Elm.Main.fullscreen({ hostname: document.location.hostname })
+chrome.tabs.query({currentWindow: true, active: true}, (tabs) => {
+    var parser = document.createElement('a')
+    parser.href = tabs[0].url
+    Elm.Main.fullscreen({ hostname: parser.hostname })
+})
